e718e4900a61f0fd4727ba26ba8996e24a1ccde9
Kris Steele <kris.steele@clockshark.com>
2022-11-10 07:26:11 -0800

    Add better class.

---
+++ GitKrakenApp/BetterClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitKrakenApp
+{
+    internal class BetterClass : ClassOne
+    {
+        public string ExtraData { get; set; }
+    }
+}
--- GitKrakenApp/Program.cs
+++ GitKrakenApp/Program.cs
@@ -9,6 +9,16 @@ namespace GitKrakenApp
             // See https://aka.ms/new-console-template for more information
             Console.WriteLine("Executing program.");
 
+            // Adding items to create a longer file.
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int d = 4;
+            int e = 5;
+            int x = 5;
+            int y = 6;
+            int z = 7;
+
             var classOne = new ClassOne();
             classOne.Description = "Description 1";
             classOne.ClassId = 1;
@@ -17,13 +27,13 @@ namespace GitKrakenApp
             classTwo.Description = "Description 2";
             classTwo.ClassId = 2;
 
-            var classFour = new ClassOne();
-            classFour.Description = "Description 4";
-            classFour.ClassId = 4;
-
             var classThree = new ClassOne();
             classThree.Description = "Description 3";
             classThree.ClassId = 3;
+
+            var classFour = new ClassOne();
+            classFour.Description = "Description 4";
+            classFour.ClassId = 4;
 
             var classes = new List<ClassOne>();
 
@@ -34,7 +44,36 @@ namespace GitKrakenApp
 
             foreach (var className in classes)
             {
-                Console.WriteLine(className.Description + Environment.NewLine);
+                Console.WriteLine("1:" + className.Description + Environment.NewLine);
+            }
+
+            foreach (var className in classes)
+            {
+                Console.WriteLine("2:" + className.Description + Environment.NewLine);
+            }
+
+            foreach (var className in classes)
+            {
+                Console.WriteLine("3:" + className.Description + Environment.NewLine);
+            }
+
+            foreach (var className in classes)
+            {
+                Console.WriteLine("4:" + className.Description + Environment.NewLine);
+            }
+
+            var betterClass = new BetterClass()
+            {
+                Description = "BetterClass",
+                ClassId = 5,
+                ExtraData = "Extra data on this class."
+            };
+
+            classes.Add(betterClass);
+
+            foreach (var className in classes)
+            {
+                Console.WriteLine("5:" + className.Description + Environment.NewLine);
             }
 
             Console.WriteLine("Thanks for playing!");
