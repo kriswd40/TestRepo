46e0084f2db7161fad4750fdce4c59c049a1790d
Kris Steele <kris.steele@clockshark.com>
2022-11-10 06:55:07 -0800

    Commit 2

---
+++ GitKrakenApp/ClasssOne.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitKrakenApp
+{
+    public class ClassOne
+    {
+        public string Description { get; set; }
+
+        public int ClassId { get; set; }
+    }
+}
--- GitKrakenApp/Program.cs
+++ GitKrakenApp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace GitKrakenApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Executing program.");
+
+            var classOne = new ClassOne();
+            classOne.Description = "Description 1";
+            classOne.ClassId = 1;
+
+            var classTwo = new ClassOne();
+            classTwo.Description = "Description 2";
+            classTwo.ClassId = 2;
+
+            var classes = new List<ClassOne>();
+
+            classes.Add(classOne);
+            classes.Add(classTwo);
+
+            foreach(var className in classes)
+            {
+                Console.WriteLine(className.Description + Environment.NewLine);
+            }
+
+            Console.WriteLine("Thanks for playing!");
+        }
+    }
+}
